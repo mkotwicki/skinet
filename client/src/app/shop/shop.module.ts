57f9f57e251b354d26f2b9fd1ca39610fc3d303c
Michał Kotwicki <michal@evol.com.pl>
2021-10-28 19:11:16 +0200

    End of section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,23 +1,21 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
-import { PagingHeaderComponent } from '../shared/components/paging-header/paging-header.component';
-
-
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ShopComponent
+    SharedModule,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
